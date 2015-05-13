184f8a4182201330f70b8564c07adde178d954dd
Abdul Hafidz A <aditans88@gmail.com>
2015-05-13 16:56:32 +0700

    angular material

--- angular/app/main.js
+++ angular/app/main.js
@@ -8,6 +8,7 @@
 		'app.directives',
 		'app.routes',
 		'app.config',
+		'app.themes'
 		]);
 
 	angular.module('app.routes', ['ui.router']);
@@ -16,5 +17,19 @@
 	angular.module('app.services', ['ui.router']);
 	angular.module('app.directives', []);
 	angular.module('app.config', []);
-
+	angular.module('app.themes', ['ngMaterial'])
+      .config(function($mdThemingProvider, $mdIconProvider){
+          $mdIconProvider
+                      .defaultIconSet("./assets/svg/avatars.svg", 128)
+                      .icon("menu"       , "./assets/svg/menu.svg"        , 24)
+                      .icon("share"      , "./assets/svg/share.svg"       , 24)
+                      .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
+                      .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
+                      .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
+                      .icon("phone"      , "./assets/svg/phone.svg"       , 512);
+          
+          $mdThemingProvider.theme('default')
+                          .primaryPalette('blue')
+                          .accentPalette('red');
+      });
 })();
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,15 @@
 <!doctype html>
 <html ng-app="app">
 <head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no" />
 	<link rel="stylesheet" href="/css/vendor.css">
 	<link rel="stylesheet" href="/css/all.css">
 	<meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
-<body>
+<body layout="column">
 
 
 	<div ui-view></div>
